d018f1e40ef84031d278dd7c2ada39076924040a
Rajeev Koushik Y G <30797103+rajeev-koushik@users.noreply.github.com>
2021-02-02 11:23:45 +0530

    V2

--- Adapt+Conversion_PDFtoJPG (Final).py
+++ Adapt+Conversion_PDFtoJPG (Final).py
@@ -1,22 +1,16 @@
 import cv2
-import numpy as np
 from matplotlib import pyplot as plt
-import pandas as pd
-from collections import namedtuple
 from PIL import Image 
 import pytesseract 
-import sys 
 from pdf2image import convert_from_path 
-import os 
 from pathlib import Path
 
-PDF_file = r"D:\OneDrive\Work\Xsaras\3. Invoice Recognition\4. Preprocessing\LR_4 Updated.pdf"
+PDF_file = r"path/to/file.pdf"
 out = Path(PDF_file).stem
 
-pages = convert_from_path(PDF_file, 300, poppler_path = r"C:\Users\PREDATOR\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.9_qbz5n2kfra8p0\LocalCache\local-packages\Python39\site-packages\poppler-20.12.1\Library\bin") 
+pages = convert_from_path(PDF_file, 300, poppler_path = r"path/to/poppler/bin/") 
 
 image_counter = 1
-
 for page in pages: 
 	filename = out+"_PDFtoJPG.jpg"
 	page.save(filename, 'JPEG')
@@ -36,9 +30,7 @@ plt.savefig(out+"_final.jpg", bbox_inches = 'tight', dpi = 300)
 outfile = out+"_final.txt"
 f = open(outfile, "a")
 
-filelimit = image_counter-1
-
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = r"path/to/tesseract.exe"
 
 filename = out+"_final.jpg"
 text = str(((pytesseract.image_to_string(Image.open(filename))))) 
